61425b0fb11484781bc99a3cef38fd8c80d2f9bd
Hezarfen <Hezarfen@DESKTOP-DBO1KQH>
2024-08-11 15:23:03 +0300

    Add project files.

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,22 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string password = textBox1.Text;
+            string customerName = textBox2.Text;
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
